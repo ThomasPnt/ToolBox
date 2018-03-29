40a871e4fb9e067de77883854406124b5c442ef1
Unknown <thomaspoint@gmail.com>
2018-03-29 16:32:33 +0200

    wip

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from "react-redux";
 import {Link} from 'react-router-dom';
-import {addCategory, filterCat, filterTool, hideAddCategory} from "../actions/ActionTool";
+import {addCategory, filterCat, hideAddCategory} from "../actions/ActionTool";
 import {bindActionCreators} from "redux";
 import {actualCategory} from "../actions/ActionCategory";
 
